78fda674f5f81e041f07fdc87122bea0fede8a50
jan <jan.abbring@e-tales.nl>
2023-01-01 20:38:17 +0100

    [#TDA-04]:+ add logger header

---
+++ src/headers/Logger.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+struct Logger
+{
+private:
+	static std::string workDir;
+	static std::ifstream readFiles;
+	static std::ofstream writefiles;
+
+public:
+	static void setWorkDir(const char* _path);
+	static void createLog(const char* _name, const char* _msg = "");
+	static void copyLog(const char* _sourcePath, const char* _destinationPath);
+	static std::string readLog(const char* _name);
+	static void writeToLog(const char* _name, const char* _msg);
+	static void removeLog(const char* _name);
+}; 
